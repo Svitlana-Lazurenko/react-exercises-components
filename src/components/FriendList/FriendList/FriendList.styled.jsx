16ec94b67367ccb1890c079e97f13306b9047014
Svitlana-Lazurenko <svitsvit222@gmail.com>
2023-05-20 07:53:59 +0300

    Fix friendlist styles

--- src/components/FriendList/FriendList/FriendList.styled.jsx
+++ src/components/FriendList/FriendList/FriendList.styled.jsx
@@ -3,4 +3,6 @@ import styled from '@emotion/styled';
 export const List = styled.ul`
   background-color: ${props => props.theme.colors.accentColor};
   margin-bottom: ${props => props.theme.spacing(15)};
+  border-radius: 25px;
+  overflow: hidden;
 `;
